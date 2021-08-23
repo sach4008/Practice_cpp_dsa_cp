b00c65da8f67c973c4b36745c6218eb550e859b9
Sachchidanand Singh <sach4008@gmail.com>
2021-08-23 12:40:02 +0530

    files added

---
+++ CSES/Number_Spiral.cpp
@@ -0,0 +1,48 @@
+#include<bits/stdc++.h>
+
+#define mod 1000000007
+#define ll long long
+using namespace std;
+
+
+
+
+
+void solve(){
+    ll x,y;
+    cin>>x>>y;
+
+    ll ans;
+
+    if (x<y){
+        if(y&1){
+            ans = y*y -x+1;
+        }
+        else{
+            ans = (y-1)*(y-1) +x;
+        }
+    }
+    else{
+        swap(x,y);
+        if(!(y&1)){
+            ans = y*y -x+1;
+        }
+        else{
+            ans = (y-1)*(y-1) +x;
+        }
+    }
+    cout<<ans<<"\n";
+
+}
+
+int main()
+{
+	ll test=1;
+	cin>>test;
+
+	while(test--){
+		solve();
+	}
+
+	return 0;
+}
